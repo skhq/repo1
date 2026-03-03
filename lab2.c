8a1579db33dc3bdcdb48b6b7502d0604fb66f7df
skhq <tudor.ganea8@yahoo.com>
2026-03-03 11:38:15 +0200

    am modificat doar recursivitatea

--- lab2.c
+++ lab2.c
@@ -1,15 +1,20 @@
 #include <stdlib.h> // Ganea Tudor luca
 #include <stdio.h>
 
-int function(int x){        // am scos apelarea recursiva pentru ca era un loop infinit 
-	x=x-2;
+int function(int x)
+{
+    if (x <= 0) // daca x = 0 ne oprim din apeluri recursive
 	return x;
+    x=x-2;
+    function(x);
+    return x;  // afisam mereu x-2
 }
 
 int main(){
 	int a=30, i=0;
 	
-	while (a>0){
+	while (a>0)
+	{
 		a=function(a);
 		i++;
 	}
